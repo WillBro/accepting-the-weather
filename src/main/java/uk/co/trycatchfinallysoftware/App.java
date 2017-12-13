a83a2c73c76b58f8a5e19d0ff032d01a015f3bca
William Brown <wm.bro.84@gmail.com>
2017-12-13 23:56:26 +0000

    Added Spring Boot Application App.

--- src/main/java/uk/co/trycatchfinallysoftware/App.java
+++ src/main/java/uk/co/trycatchfinallysoftware/App.java
@@ -1,13 +1,26 @@
 package uk.co.trycatchfinallysoftware;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean(name = "chromeDriver")
+    public ChromeDriver getChromeDriver() throws InstantiationException, IllegalAccessException {
+        ChromeDriverManager.getInstance().setup();
+
+        Class<? extends WebDriver> driverClass = ChromeDriver.class;
+        ChromeDriverManager.getInstance().setup();
+
+        return (ChromeDriver) driverClass.newInstance();
     }
 }
